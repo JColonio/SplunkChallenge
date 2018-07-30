fc5cb5e951007c55e492cfeaa727f2b4d9007c53
John Colonio <colonij21@gmail.com>
2018-07-29 23:59:00 -0700

    adding new sorting test
    verify sorting based on genre_id and Ids.

--- src/test/java/tests/testing.java
+++ src/test/java/tests/testing.java
@@ -5,9 +5,7 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -94,9 +92,9 @@ public class testing extends testBase {
 
         Response response = getRequest("https://splunk.mocklab.io/movies");
         //Get list of all poster_path from response
-        List<String> allPosterPath =  response.jsonPath().getList("results.poster_path");
+        List<String> allPosterPath = response.jsonPath().getList("results.poster_path");
         //loop through and verify each poster_path is not null
-        for (String posterPath : allPosterPath){
+        for (String posterPath : allPosterPath) {
             Assert.assertNotNull(posterPath, "poster_path is null");
         }
     }
@@ -110,11 +108,11 @@ public class testing extends testBase {
 
         Response response = getRequest("https://splunk.mocklab.io/movies");
         //Get list of all poster_path from response
-        List<String> allPosterPath =  response.jsonPath().getList("results.poster_path");
+        List<String> allPosterPath = response.jsonPath().getList("results.poster_path");
 
         //loop through each PosterPath and assign to hashset uniqueList
         //  if the img url already exist then add value to dupList
-        for(String posterPath : allPosterPath) {
+        for (String posterPath : allPosterPath) {
             if (!uniqueList.add(posterPath)) {
                 dupList.add(posterPath);
             }
@@ -126,14 +124,14 @@ public class testing extends testBase {
     }
 
     @Test
-    public void testMovieTitlePalindrome () {
+    public void testMovieTitlePalindrome() {
 
         String[] words;
         Set<String> palindromList = new HashSet<String>();
 
         Response response = getRequest("https://splunk.mocklab.io/movies");
         //Get list of all poster_path from response
-        List<String> allTitles =  response.jsonPath().getList("results.title");
+        List<String> allTitles = response.jsonPath().getList("results.title");
 
         //loop through each title
         for (String title : allTitles) {
@@ -148,5 +146,43 @@ public class testing extends testBase {
         Assert.assertTrue(palindromList.size() > 0, "Not one palindrome was found");
     }
 
+
+    @Test
+    public void testResultsSorting() {
+
+        Response response = getRequest("https://splunk.mocklab.io/movies");
+        //Get list of all poster_path from response
+        List<String> allGenreIds = response.jsonPath().getList("results.genre_ids");
+        List<String> allIds = response.jsonPath().getList("results.id");
+        boolean result = false;
+
+        for (int i = 1; i < allGenreIds.size(); i++) {
+            String previous = Collections.singleton(allGenreIds.get(i - 1)).toString().replaceAll("\\[", "").replaceAll("\\]","");;
+            String next = Collections.singleton(allGenreIds.get(i)).toString().replaceAll("\\[", "").replaceAll("\\]","");;
+
+            //if the first point has values & the 2nd pointer is empty
+            //      items are not sorted. Failed
+            if ( (previous.length() > 0 )&&(next.length() == 0) ){
+                result = false;
+                break;
+            }
+            //else if both are empty, check sorting by ids
+            else if ( (previous.length() == 0 )&&( next.length() == 0 ) ) {
+                if (allIds.get(i-1).compareTo(allIds.get(i)) < 0) {
+                    result = false;
+                    break;
+                }
+                else {
+                    result = true;
+                }
+            }
+            else {
+                result = true;
+            }
+        }
+
+        Assert.assertTrue(result, "list is not sorted by gener_id or ids");
+
+    }
 
 }
